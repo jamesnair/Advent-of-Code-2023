b1b4e6f8578be9fd48b6fe71518d60425d65198c
James Nair <james.nair@fieldsolutions-group.com>
2023-12-03 21:27:22 +1000

    moved things to helpers

--- src/day2/part1.ts
+++ src/day2/part1.ts
@@ -1,28 +1,4 @@
-export const colors = ["red", "blue", "green"] as const;
-type Color = (typeof colors)[number];
-
-const Bag: Record<Color, number> = {
-  red: 12,
-  blue: 14,
-  green: 13,
-};
-
-const getMarblesForSet = (set: string): Record<Color, number> => {
-  let dict: Record<Color, number> = {
-    red: 0,
-    blue: 0,
-    green: 0,
-  };
-  let colorNum = set.split(",");
-  colorNum.forEach((combo) => {
-    combo = combo.trim();
-    let number = combo.split(" ")[0];
-    let color = combo.split(" ")[1] as Color;
-    dict[color] = parseInt(number);
-  });
-
-  return dict;
-};
+import { getMarblesForSet, Bag } from "./helpers";
 
 export function computeGame(gameString: string) {
   let stringArr = gameString.split(":");
